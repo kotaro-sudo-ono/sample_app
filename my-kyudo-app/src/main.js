38196da3d65971ab36060d37b76ada78f1787b13
kotaro.ono <kotaro.ono@optim.co.jp>
2024-11-24 22:49:14 +0900

    vuetifyのインポート

--- my-kyudo-app/src/main.js
+++ my-kyudo-app/src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
+import { createApp, useAttrs } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router';
+import vuetify from './plugins/vuetify'
 
-const app = createApp(App).mount('#app')
+
+
+const app = createApp(App).use(vuetify).mount('#app')
