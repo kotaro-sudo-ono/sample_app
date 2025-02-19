73d15a4a5d6ac038125d0cdad436797f7783abd0
kotaro.ono <kotaro.ono@optim.co.jp>
2025-02-19 21:29:51 +0900

    ダイアログのテンプレートを実装
    feature/7
    
    すでに使っていたコンポーネントでエラーが出ないように修正

--- my-kyudo-app/src/components/shared/dialogTemplate/DialogTemplate.vue
+++ my-kyudo-app/src/components/shared/dialogTemplate/DialogTemplate.vue
@@ -1,18 +1,33 @@
-<script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
-import Button from '/Users/opm007852/kotaro.ono/sample_app/my-kyudo-app/src/components/Button.vue';
+<script setup lang="ts">
+import { ref } from 'vue';
+import Button from '../button/Button.vue';
 
-const showDialog = ref(false);
+const props = defineProps({
+  dialogTitle: {
+    type: String,
+    default: 'ダイアログタイトル'
+  },
+  modelValue: {
+    type: Boolean,
+    required: true
+  }
+});
 
-const isShowDialog = () => {
-  showDialog.value = !showDialog.value;
-};
+const emit = defineEmits<{
+  cancel: [];
+}>();
+
+const close = () => {
+  emit('cancel')
+}
+
 const triggerShake = ref(false); // 振動状態を管理
 const dialogElement = ref(null); // ダイアログ要素を参照するための変数
 
 // ダイアログ外またはオーバーレイ部分をクリックした場合に振動
-const overlayClick = (event) => {
-  const clickDialogOutside = event.target.closest('.dialog');
+const overlayClick = (event: MouseEvent) => {
+  const target = event.target as HTMLElement;
+  const clickDialogOutside = target.closest('.dialog');
   if (!clickDialogOutside) {
     triggerShake.value = true;
     setTimeout(() => {
@@ -20,47 +35,38 @@ const overlayClick = (event) => {
     }, 500);
   }
 };
-const handleOutsideClick = (event) => {
-  if (!dialogElement.value.contains(event.target) && !showDialog) {
-    console.log('aaa');
-    showDialog.value = false;
-  }
-};
-// コンポーネントがマウントされたときにイベントリスナーを追加
-onMounted(() => {
-  window.addEventListener('click', handleOutsideClick);
-});
-
-// コンポーネントがアンマウントされたときにイベントリスナーを削除
-onUnmounted(() => {
-  window.removeEventListener('click', handleOutsideClick);
-});
 </script>
 
 <template>
-  <div v-show="showDialog" class="overlay" @click="overlayClick"></div>
-
+  <div v-show="modelValue" class="overlay" @click="overlayClick"></div>
   <div
-    v-show="showDialog"
+    v-show="modelValue"
     ref="dialogElement"
     :class="{ dialog: true, shake: triggerShake }"
   >
-    <h2>ダイアログタイトル</h2>
-    <slot>
-      <p>ダイアログの内容...</p>
-    </slot>
-    <Button @click="isShowDialog" :text="'ダイアログを閉じる'" />
-  </div>
-  <div v-show="!showDialog">
-    <Button @click="isShowDialog" :text="'ダイアログを開く'" />
+    <div>
+      <h2>{{dialogTitle}}</h2>
+    </div>
+    <div class="content">
+      <slot name="content" >
+        <p>ダイアログの内容...</p>
+      </slot>
+    </div>
+    <div class="close">
+      <Button @click="close" :text="'ダイアログを閉じる'" />
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+$header_height: 30%; 
+$content_height: 60%;  
+$close_height: 10%; 
+
 .overlay {
   position: fixed;
-  width: 50%;
-  height: 50%;
+  width: 75%;
+  height: 75%;
   background: rgba(0, 0, 0, 0.5);
   z-index: 999;
 }
@@ -73,10 +79,33 @@ onUnmounted(() => {
   background-color: white;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   z-index: 1000; /* オーバーレイより上に表示されるように */
-  width: 300px;
+  width: 50%;
+  height: 50%;
   text-align: center;
   border: 1px solid #1867c0;
   border-radius: 18px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.header {
+  height: $header_height;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+}
+
+.content {
+  height: $content_height;
+}
+
+.close {
+  height: $close_height;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .shake {
   animation: shakeAnimation 2s infinite;
@@ -105,28 +134,4 @@ onUnmounted(() => {
     margin-left: 0px;
   }
 }
-
-/* transformはtransform: translate(-50%, -50%)で位置を変更するため要素がずれる
-  0% {
-    transform: translate(2px, 0px);
-  }
-  5% {
-    transform: translate(-2px, 0px);
-  }
-  10% {
-    transform: translate(2px, 0px);
-  }
-  15% {
-    transform: translate(-2px, 0px);
-  }
-  20% {
-    transform: translate(2px, 0px);
-  }
-  25% {
-    transform: translate(-2px, 0px);
-  }
-  30% {
-    transform: translate(0px, 0px);
-  }
-} */
 </style>
--- my-kyudo-app/src/components/signIn/SignIn.vue
+++ my-kyudo-app/src/components/signIn/SignIn.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { ref } from 'vue';
 import DialogTemplate from '../shared/dialogTemplate/DialogTemplate.vue';
-import PageTemplate from '../PageTemplate.vue';
-import Button from '../Button.vue';
+import PageTemplate from '../shared/pageTemplate/PageTemplate.vue';
+import Button from '../shared/button/Button.vue';
 
-const visible = ref(false); // ダイアログの表示状態
-const openDialog = () => {
+const visible = ref(false);
+const showDialog = () => {
   console.log('Button clicked');
   visible.value = !visible.value;
 };
@@ -14,10 +14,10 @@ const openDialog = () => {
 <template>
   <PageTemplate>
     <template #top-left>
-      <Button @click="openDialog" :text="'ダイアログを使用する'" />
+      <Button @click="showDialog" :text="'Sing Inダイアログを開く'" />
     </template>
     <template #content-center>
-      <DialogTemplate v-if="visible" />
+      <DialogTemplate v-if="visible" :model-value="visible" dialog-title="Sign In" @cancel="showDialog"/>
     </template>
   </PageTemplate>
 </template>
